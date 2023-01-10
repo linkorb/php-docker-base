81e04a7e9b0d2a6a2c242e73376138e67139f60d
Aike de Jongste <aikedejongste@gmail.com>
2023-01-10 17:05:00 +0100

    fix: remove db migrations and db wait from entrypoint
    now similar to original https://github.com/docker-library/php/blob/master/8.1/buster/apache/docker-php-entrypoint

--- app-docker-entrypoint.sh
+++ app-docker-entrypoint.sh
@@ -1,18 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/sh
 set -e
 
-if [ "$1" = 'apache2-foreground' ]; then
-
-  until bin/console doctrine:database:create --quiet --no-interaction --if-not-exists; do
-    >&2 echo "Database is unavailable - sleep for 10s"
-    sleep 10
-  done
-
-  >&2 echo "The database is up - resuming"
-  bin/console doctrine:migrations:migrate --no-interaction \
-    && vendor/bin/envoi validate \
-    && APP_ENV=$APP_ENV apache2-foreground
-
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- apache2-foreground "$@"
 fi
 
 exec "$@"
